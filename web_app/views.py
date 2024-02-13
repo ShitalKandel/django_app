d0ef12eeeae27e39bad02afdaba37881f9da08bd
Shital Kandel <shital@Kali.Kali>
2024-02-13 13:09:57 +0545

    add some comments

--- web_app/views.py
+++ web_app/views.py
@@ -4,21 +4,24 @@ from .models import UserProfile,Photo, Feeds
 from .forms import SignupForm,LoginForm,UserImage,New_post
 
 
+'''
+register a account using django form
+'''
 def register(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-
         if form.is_valid():
             form.save()   
             return redirect('web_app:login_success')  
     else:
         form = SignupForm()
-    
     return render(request, 'register.html', {'form': form})
 
 
 
-
+'''
+after logging in it will redirect to a new page
+'''
 def login_success(request):
     form = UserImage(request.POST, request.FILES)
     if form.is_valid():
@@ -26,6 +29,10 @@ def login_success(request):
     return render(request, 'login_success.html',{'form':form})
 
 
+'''
+it verifies the user and redirects to login_success 
+if the user is valid
+'''
 def signIn(request):
     error_message = None
     if request.method == 'POST':
@@ -43,32 +50,41 @@ def signIn(request):
     return render(request, 'login.html', {'form': form, 'error_message': error_message})
 
 
+"""
+it redirects to the login_success
+"""
 def profile(request):
     return redirect(request,'login_success')
 
+
+'''
+User logout form login_success page
+'''
 def logout(request):
     
     return render(request,'logout.html')
 
 
+'''
+this field is created to upload image with caption
+'''
 def image_request(request):
     image = UserImage.objects.__dict__.get.__all__
-
     form = UserImage()
     if request.method == 'POST':
         form = UserImage(request.POST, request.FILES)    
-        
         if form.is_valid():
             image = form.save()
-
             return render(request,'image.html',{'image':image})
-        
     else:
         form = UserImage()
 
     return render(request,'image.html',{'form':form})
 
 
+'''
+it uplods the image
+'''
 def upload_image(request,image_id):
     try:
         image = Photo.objects.get(pk=image_id)
@@ -86,9 +102,20 @@ def upload_image(request,image_id):
         return render(request,'image.html',{'form':form,'image':image})
     
 
+'''
+creating a post on the login_success page'''
 def create_post(request):
-    return render(request,'home_page.html')
+    if  request.method =="POST":
+        form = Photo()
+        if form.is_valid():
+            form.save()
+        return redirect(request,'post')
 
+
+'''
+displays the created post on feeds of the page
+with the username , comment , number of likes ,caption or image
+'''
 def post(request, first_name, last_name):
     user_profile = UserProfile.objects.get(first_name=first_name, last_name=last_name)
     if request.method == 'POST':
@@ -106,9 +133,11 @@ def post(request, first_name, last_name):
 
 
 
+'''profiel bar '''
 def left_Profile_Bar(request):
     return render(request,'left_side_profilebar.html' )
 
 
+'''settings bar'''
 def right_Profile_Bar(request):
     return render(request,'right_side_profilebar.html' )
