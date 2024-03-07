3dbed2459a8ef9c0a53de04d47ae82380c8aef74
Shital Kandel <shital@Kali.Kali>
2024-03-07 12:36:04 +0545

    Test commit 3

--- crud/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from crud import views
-
-
-router = routers.SimpleRouter()
-router.register = (r'articles' , views.ArticleViewSets().as_view())
-router.register = (r'author',views.AuthorView().as_view())
-urlpatterns = router.urls
